95a3b183a6cf8a76af0e9e9b309567426b3a812d
XunzhaoYu <yuxunzhao@gmail.com>
2017-12-03 23:41:17 +0000

    Convert assembly programmes into hash dictionary

---
+++ kgram.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# The steps of calculate hash for similarity check:
+# 1. pre-processing
+# 2. dividing the files by K-gram. List kGram.
+# 3. calculating the hash value of kGram. List hashList.
+# 4. shrinking the size of list by winnowing algorithm. Dictionary winHash.
+
+import os
+import time
+import datetime
+import timeit
+
+K = 5
+data = ""
+
+readPath = "/Users/xunzhaoyu/Documents/PhD/Documents of Study/TA/submissions/submissions_pre/"
+writePath = "/Users/xunzhaoyu/Documents/PhD/Documents of Study/TA/submissions/submissions_hash/"
+
+fileList = os.listdir(readPath)
+fileList = fileList[1:len(fileList)]  # remove the hidden file ".DS_Store"
+print fileList
+
+count = 0;
+
+for f in fileList:
+    print f
+    count += 1
+    print count
+    fileReadPath = os.path.join(readPath, f)
+    fileWritePath = os.path.join(writePath, f)
+
+    if os.path.isfile(fileReadPath):
+        srcFile = open(fileReadPath)
+        objFile = open(fileWritePath, "w")
+
+        data = srcFile.read()
+
+# Step2: k-gram division -- kGram
+        kGram = []
+
+        shingleNum = len(data)-K
+        for i in range(0, shingleNum):
+            shingle = data[i:i+K]
+            kGram.append(shingle)
+
+        print kGram
+        print "the number of k-gram:" + str(len(kGram)) + ", " + str(shingleNum)
+
+# Step3: rolling hash -- hashList
+        Base = 3
+        first_hash = 0
+        pre_hash = 0
+        hash = 0
+        hashList = []
+        firstShingle = kGram[0]
+
+#        start = time.time()
+#        stTime = datetime.datetime.now()
+
+        for i in range(K):
+            hash += ord(firstShingle[i])*(Base**(K-1-i))
+
+        hashList.append(hash)
+
+        for i in range(1, len(kGram)):
+            preshingle = kGram[i-1]
+            shingle = kGram[i]
+
+            hash = hash * Base - ord(preshingle[0])*Base**K + ord(shingle[K-1])
+            hashList.append(hash)
+
+        print hashList
+
+#        end = time.time()
+#        endTime = datetime.datetime.now()
+
+#        print "rolling Hash running time :" + str((end-start))
+
+# Step4: winnowing hash -- winHash
+
+        WINSIZE = 4
+        winCnt= len(kGram)-WINSIZE+1
+
+        minHash = 0
+        minPos = 0
+        winHash = {}
+
+        for i in range(winCnt):
+            templist = hashList[i:WINSIZE+i]
+
+            minHash= templist[WINSIZE-1]
+            minPos = WINSIZE+i-1
+
+            for j in range(WINSIZE):
+
+                if templist[j] < minHash:
+                    minHash = templist[j]
+                    minPos = i+j
+
+            if not winHash.has_key(minPos):
+                winHash[minPos] = minHash
+
+        print winHash
+
+        objFile.write(str(winHash))
+
+        srcFile.close()
+        objFile.close()
---
+++ pre_processing_assembly.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# The steps of calculate hash for similarity check:
+# 1. pre-processing
+# 2. dividing the files by K-gram. List kGram.
+# 3. calculating the hash value of kGram. List hashList.
+# 4. shrinking the size of list by winnowing algorithm. Dictionary winHash.
+
+import os
+import time
+import datetime
+import timeit
+
+# Step 1: pre-processing:
+
+readPath = "/Users/xunzhaoyu/Documents/PhD/Documents of Study/TA/submissions/submissions/"
+writePath = "/Users/xunzhaoyu/Documents/PhD/Documents of Study/TA/submissions/submissions_pre/"
+
+fileList = os.listdir(readPath)
+fileList = fileList[1:len(fileList)]  # remove the hidden file ".DS_Store"
+
+comment = "#"
+for f in fileList:
+    fileReadPath = os.path.join(readPath, f)
+    fw = f[0:len(f)-1] + "txt"
+    fileWritePath = os.path.join(writePath, fw)
+
+    if os.path.isfile(fileReadPath):
+        srcFile = open(fileReadPath)
+        objFile = open(fileWritePath, "w")
+
+        # start pre-processing: ---------------------------------------------------------------------
+        srcData = ""
+        afterMain = False
+        labels = []
+
+        fileLines = srcFile.readlines()
+        for line in fileLines:
+            # ignore .data section
+            if not afterMain:
+                # afterMain = (line.strip()[0:5] == "main:")
+                if line.find("main") != -1:
+                    afterMain = True
+
+            # start process main code
+            else:
+                # delete comments
+                commentPos = line.find(comment)
+                if commentPos != -1:
+                    line = line[0:commentPos] + "\n"
+
+                # record labels
+                if line.find(":") != -1:
+                    temp = line.strip()
+                    labels.append(temp[0:len(temp) - 1])
+                    line = ""
+
+                # add lines into data
+                srcData = srcData + line
+
+        # sort labels by length
+        labelNum = len(labels)
+###        print labelNum
+
+        swap = True
+        while swap:
+            swap = False
+            for i in range(1, labelNum):
+                if swap:
+                    break
+                else:
+                    swap = False
+                for j in range(0, labelNum - i):
+                    if len(labels[j]) < len(labels[j + 1]):
+                        temp = labels[j]
+                        labels[j] = labels[j + 1]
+                        labels[j + 1] = temp
+                        swap = True
+        # delete labels
+        for label in labels:
+###            print label
+            srcData = srcData.replace(label, "")
+
+        # abbreviate special instruction: syscall
+        srcData = srcData.replace("syscall", "sys")
+
+        # delete all space, comma
+        srcData = srcData.replace(",", "").replace(";", "")
+        objData = "".join(srcData.split())
+
+        # end of pre-processing: ---------------------------------------------------------------------
+        objFile.write(objData)
+
+        srcFile.close()
+        objFile.close()
